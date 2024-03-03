a8ac0ee008f6024bad5bec02b6799282d846d56a
mariocb-duarte <mcbd.eng@gmail.com>
2024-03-03 16:01:09 -0300

    update

---
+++ src/main/java/com/example/padroesdeprojeto/service/ClienteService.java
@@ -0,0 +1,16 @@
+package com.example.padroesdeprojeto.service;
+
+import com.example.padroesdeprojeto.model.Cliente;
+
+public interface ClienteService {
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
